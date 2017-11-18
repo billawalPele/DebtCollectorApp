09bb2597fd5376094674dc6e700ac96ba5ecadb7
DESKTOP-8O5QK3T\devel <developer.hilmi@gmail.com>
2017-11-18 12:58:50 +0700

    Initialize

---
+++ app/src/main/java/com/rizaldev/debtcollector/screens/utang_baru/UtangBaruActivity.java
@@ -0,0 +1,15 @@
+package com.rizaldev.debtcollector.screens.utang_baru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rizaldev.debtcollector.R;
+
+public class UtangBaruActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_utang_baru);
+    }
+}
