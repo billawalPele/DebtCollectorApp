d0e81b4ed1da865eec880cca01850bf3bcb99908
DESKTOP-8O5QK3T\devel <developer.hilmi@gmail.com>
2017-11-18 13:59:50 +0700

    Add Smart Adapter

--- app/src/main/java/com/rizaldev/debtcollector/screens/bayar_utang/BayarUtangActivity.java
+++ app/src/main/java/com/rizaldev/debtcollector/screens/bayar_utang/BayarUtangActivity.java
@@ -1,15 +1,15 @@
-package com.rizaldev.debtcollector.screens.login;
+package com.rizaldev.debtcollector.screens.bayar_utang;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rizaldev.debtcollector.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class BayarUtangActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_bayar_utang);
     }
 }
--- app/src/main/java/com/rizaldev/debtcollector/screens/main/MainActivity.java
+++ app/src/main/java/com/rizaldev/debtcollector/screens/main/MainActivity.java
@@ -1,15 +1,15 @@
-package com.rizaldev.debtcollector.screens.login;
+package com.rizaldev.debtcollector.screens.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rizaldev.debtcollector.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
     }
 }
